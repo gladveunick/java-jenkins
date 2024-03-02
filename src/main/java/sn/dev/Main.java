fb154c552c613d2c5e954b7dfdea93e7293f7600
gladngoyi <gladveunick@gmail.com>
2024-03-02 22:01:56 +0000

    first commit

---
+++ src/main/java/sn/dev/Main.java
@@ -0,0 +1,9 @@
+package sn.dev;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        System.out.println("Les choses se passent!");
+    }
+}
